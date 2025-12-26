9447946e410d00d6c4e844ad97072e1519fc050b
moiz-260 <moiz.khan@mergestack.com>
2025-12-26 16:23:39 +0500

    Update SignUpForm.tsx

--- src/components/auth/signup/SignUpForm.tsx
+++ src/components/auth/signup/SignUpForm.tsx
@@ -140,7 +140,7 @@ const SignUpForm: React.FC = () => {
     };
 
     return (
-        <div className="flex flex-col gap-6 w-full">
+        <div className="flex flex-col gap-6 w-full max-w-md mx-auto">
             <div className="flex flex-col gap-6">
                 <RiveTeddyAnimation ref={riveRef} />
 
